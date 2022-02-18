57aec56bdc475d33f6bc3c6e3d71f4ef15eed14a
askarbek101 <askarbek101@gmail.com>
2022-02-19 00:10:08 +0600

    Add models.

---
+++ OnlineShopDemo/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
